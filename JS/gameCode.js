4a04ee31cfee35f4790e80dbc929af06c0d9e914
Cruz <cgonz1093@gmail.com>
2022-08-02 08:29:34 -0600

    Update style, adjust code to work with webpage and buttons, realign header

--- JS/gameCode.js
+++ JS/gameCode.js
@@ -1,29 +1,20 @@
+const playGameBtn = document.getElementById('playGame');
+const rockBtn = document.getElementById('rockButton');
+const paperBtn = document.getElementById('paperButton');
+const scissorsBtn = document.getElementById('scissorsButton');
+
 // Function to setup computer play of rock, paper and scissors via randomly choosing from an array
 function computerPlay() {
     playType = ['rock','paper','scissors']
-    const result = playType[Math.floor(Math.random()* 2.5)]
-    return result
-}
-// Function to let the user choose an input
-function userPlay() {
-  chosenType = prompt('Choose rock, paper or scissors','').toLowerCase()
-  if (isNaN(chosenType)) {
-    if (chosenType === 'rock' || chosenType === 'paper' || chosenType === 'scissors') {       
-      return chosenType
-    } else {
-      alert('Please enter either: rock, paper or scissors')
-    }
-  } else {
-    alert('Please enter either: rock, paper or scissors')
-  }
+    return result = playType[Math.floor(Math.random()* 2.5)]
 }
 // variables to count wins
 let compWins = 0
 let userWins = 0
 // function that plays a round of rock,paper scissors
-function playRound(computerPlay,userPlay) {
-  if (computerPlay != userPlay) {
-    if (userPlay === 'rock') {
+function playRound(computerPlay,userChoice) {
+  if (computerPlay != userChoice) {
+    if (userChoice === 'rock') {
         if (computerPlay === 'paper') {
           compWins += 1
           console.log('You lose! Paper beats rock.')
@@ -31,7 +22,7 @@ function playRound(computerPlay,userPlay) {
           userWins += 1
           console.log('You win! Rock beats scissors.')
         }
-    } else if (userPlay === 'paper') {
+    } else if (userChoice === 'paper') {
         if (computerPlay ==='rock') {
           userWins += 1
           console.log('You win! Paper beats rock!')
@@ -39,7 +30,7 @@ function playRound(computerPlay,userPlay) {
           compWins += 1
           console.log('You lose! Scissors beat paper.')
         }
-    } else if (userPlay === 'scissors') {
+    } else if (userChoice === 'scissors') {
         if (computerPlay === 'rock') {
           compWins += 1
           console.log('You lose! Rock beats scissors.')
@@ -52,32 +43,46 @@ function playRound(computerPlay,userPlay) {
     console.log('The round is a draw!')
   }
 }
-// function to ask the user how many times they'd like to play and parse the int
-function timesToPlay() {
-  let timesToPlay = parseInt(prompt('How many times do you wish to play?',1))
-  if (isNaN(timesToPlay)) {
-    alert('Not a number. Please enter a number.')
-  } else {
-    return timesToPlay
-  }
-}
-// function to play the game n amount of times while recording wins and losses
-function game() {
-  let iterations = timesToPlay()
-  for (let i = 0; i < iterations;i++) {
-      playRound(computerPlay(),userPlay())
-  }
-  if (userWins > compWins) {
-    console.log(`You have ${userWins} win's to the computers ${compWins} wins, you win! Play again?`)
-  } else if (compWins > userWins) {
-    console.log(`The score is: You ${userWins} win's to the computers ${compWins} wins, you lose. Try again?`)
-  } else {
-    console.log('You tied! Try again?')
-  }
-  resetGame()
-}
+
 // function to reset game
 function resetGame() {
   compWins = 0
   userWins = 0
 }
+
+function buttonSwap() {
+  rockBtn.style.display = 'inline-block';
+  paperBtn.style.display = 'inline-block';
+  scissorsBtn.style.display = 'inline-block'
+  playGameBtn.style.display = 'none'
+}
+
+function textChange() {
+  const gameTextContainer = document.querySelector('.gameStatHolder')
+  const gameText = document.createElement('p')
+  gameText.classList.add('gameText')
+  gameText.textContent= `Computer wins: ${compWins}` + '\r\n' + `Player wins: ${userWins}`
+  gameText.style.fontSize = '28px'
+  gameText.style.color = 'white'
+  gameText.style.fontWeight = 'bold'
+  gameTextContainer.appendChild(gameText)
+}
+function startGame() {
+  buttonSwap();
+  textChange();
+}
+playGameBtn.addEventListener('click',startGame)
+
+rockBtn.addEventListener('click',()=>{
+  let userChoice = 'rock'
+  playRound(computerPlay(),userChoice)
+})
+paperBtn.addEventListener('click',()=>{
+  let userChoice = 'paper'
+  playRound(computerPlay(),userChoice)
+})
+scissorsBtn.addEventListener('click',() => {
+  let userChoice = 'scissors'
+  playRound(computerPlay(),userChoice)
+})
+
